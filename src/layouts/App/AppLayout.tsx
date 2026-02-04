f3ffcf385673bc626f763022805983fd52a46e6b
LucaBn <luca.bonotti@virgilio.it>
2026-02-04 01:53:37 +0100

    feat(AppLayout): :sparkles: wrap component in AppShell

--- src/layouts/App/AppLayout.tsx
+++ src/layouts/App/AppLayout.tsx
@@ -1,4 +1,4 @@
-import { Container } from "@mantine/core";
+import { AppShell, Container } from "@mantine/core";
 import { Outlet } from "react-router-dom";
 
 import { Header } from "@/layouts/App/Header";
@@ -7,11 +7,11 @@ import styles from "./AppLayout.module.scss";
 
 export const AppLayout: React.FC = () => {
   return (
-    <>
+    <AppShell>
       <Header />
       <Container className={styles.appContainer} fluid>
         <Outlet />
       </Container>
-    </>
+    </AppShell>
   );
 };
